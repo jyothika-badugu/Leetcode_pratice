2acd6e11f5874ce3676de271791e8f674b326b9f
Your Name <badugusambaih61@gmail.com>
2025-08-24 21:39:44 +0530

    return power

---
+++ pow(x,n).py
@@ -0,0 +1,8 @@
+class solution:
+    def pow(self,n,x):
+        return n**x
+n=int(input(" enter val n "))
+x=int(input("enter val x "))
+sol = solution() #object creation
+print(sol.pow(n, x)) #call function3
+
